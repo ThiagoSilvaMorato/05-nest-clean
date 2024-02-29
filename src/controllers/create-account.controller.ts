9b116335818d1df78c52411abb560f73a95db7bb
Thiago <thiagosmorato@gmail.com>
2024-02-29 16:28:53 -0300

    Fundamentos do Nest: Gerando hash da senha

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,6 @@
 import { Body, ConflictException, Controller, HttpCode, Post } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { hash } from "bcryptjs";
 
 @Controller("/accounts")
 export class CreateAccountController {
@@ -20,11 +21,13 @@ export class CreateAccountController {
       throw new ConflictException("User with same e-mail address already exists.");
     }
 
+    const hashedPassword = await hash(password, 8);
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     });
   }
