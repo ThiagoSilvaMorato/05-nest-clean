d84202b4d1f6a58a5760690178b242e36921e102
Thiago <thiagosmorato@gmail.com>
2024-03-07 10:21:47 -0300

    Desacoplando camadas no NestJS: Criando camada de Infraestrutura

---
+++ src/infra/app.module.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaService } from "./prisma/prisma.service";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
+import { HttpModule } from "./http/http.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    HttpModule,
+  ],
+})
+export class AppModule {}
