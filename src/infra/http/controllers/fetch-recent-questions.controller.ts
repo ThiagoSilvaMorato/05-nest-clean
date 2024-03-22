c5263e6749b0fa15d4cfbc80d2a80e95edc6568c
Thiago <thiagosmorato@gmail.com>
2024-03-22 11:01:30 -0300

    Desacoplando camadas do NestJS: Rotas privadas por padrão

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -18,7 +18,6 @@ const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
 type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
 
 @Controller("/questions")
-@UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
   constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
